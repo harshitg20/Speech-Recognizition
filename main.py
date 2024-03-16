9af759f561454f38c97188bd92b0a1482a9925fd
Harshit Gupta <142491130+harshitg20@users.noreply.github.com>
2024-03-16 08:36:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -121,10 +121,7 @@ def open_about_page():      # about page
     about_window.title("About")
     about_window.iconphoto(False, icon)
 
-    # Create a link to the GitHub repository
-    github_link = ttk.Label(about_window, text="github.com/SamirPaulb/real-time-voice-translator", underline=True, foreground="blue", cursor="hand2")
-    github_link.bind("<Button-1>", lambda e: open_webpage("https://github.com/SamirPaulb/real-time-voice-translator"))
-    github_link.pack()
+
 
     about_text = tk.Text(about_window, height=10, width=50)
     about_text.insert("1.0", """
