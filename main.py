e3046c98d40f428a97720fbec74b6a030a3e3c4a
Harshit Gupta <142491130+harshitg20@users.noreply.github.com>
2024-03-16 08:00:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,16 @@ import threading
 import tkinter as tk
 from gtts import gTTS
 from tkinter import ttk
-import speech_recognition as sr
-from playsound import playsound
 
 
-# Create an instance of Tkinter frame or window
+
 win= tk.Tk()
 
-# Set the geometry of tkinter frame
 win.geometry("700x450")
 win.title("Real-Time Voice🎙️ Translator🔊")
 icon = tk.PhotoImage(file="icon.png")
 win.iconphoto(False, icon)
 
-# Create labels and text boxes for the recognized and translated text
 input_label = tk.Label(win, text="Recognized Text ⮯")
 input_label.pack()
 input_text = tk.Text(win, height=5, width=50)
@@ -30,7 +26,6 @@ output_text.pack()
 blank_space = tk.Label(win, text="")
 blank_space.pack()
 
-# Create a dictionary of language names and codes
 language_codes = {
     "English": "en",
     "Hindi": "hi",
@@ -38,7 +33,6 @@ language_codes = {
 
 language_names = list(language_codes.keys())
 
-# Create dropdown menus for the input and output languages
 
 input_lang_label = tk.Label(win, text="Select Input Language:")
 input_lang_label.pack()
@@ -132,14 +126,12 @@ def open_about_page():      # about page
     github_link.bind("<Button-1>", lambda e: open_webpage("https://github.com/SamirPaulb/real-time-voice-translator"))
     github_link.pack()
 
-    # Create a text widget to display the about text
     about_text = tk.Text(about_window, height=10, width=50)
     about_text.insert("1.0", """
     A machine learning project that translates voice from one language to another in real time while preserving the tone and emotion of the speaker, and outputs the result in MP3 format. Choose input and output languages from the dropdown menu and start the translation!
     """)
     about_text.pack()
 
-    # Create a "Close" button
     close_button = tk.Button(about_window, text="Close", command=about_window.destroy)
     close_button.pack()
 
@@ -149,11 +141,9 @@ def open_webpage(url):      # Opens a web page in the user's default web browser
 
 
 
-# Create the "Run" button
 run_button = tk.Button(win, text="Start Translation", command=run_translator)
 run_button.place(relx=0.25, rely=0.9, anchor="c")
 
-# Create the "Kill" button
 kill_button = tk.Button(win, text="Kill Execution", command=kill_execution)
 kill_button.place(relx=0.5, rely=0.9, anchor="c")
 
@@ -164,3 +154,5 @@ from deep_translator import GoogleTranslator
 from google.transliteration import transliterate_text
 # Run the Tkinter event loop
 win.mainloop()
+import speech_recognition as sr
+from playsound import playsound
